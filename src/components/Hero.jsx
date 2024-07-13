495c39bf02887c67490b18f3d7a1c0322db9ce2a
SidhardhCR <sidhardhcr007@gmail.com>
2024-07-13 13:02:26 +0530

    home page updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
-
+import { useMediaQuery } from "@uidotdev/usehooks";
 
 
 function Hero() {
+  const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
   return (
     <div className='hero'>
         <div className='header'>
             <h1 className='myname' >SIDHARDH C R</h1>
-            <div className='menu'>
-                <p>Home</p>
-                <p>About</p>
-                <p>Skills</p>
-                <p>Experience</p>
-                <p>Contact</p>
-            </div>
+            {
+              isSmallDevice? <div style={{marginTop:'0.3em'}}><svg xmlns="http://www.w3.org/2000/svg" width="1.7em" height="1.7em" viewBox="0 0 24 24"><path fill="white" d="M3 18v-2h18v2zm0-5v-2h18v2zm0-5V6h18v2z"></path></svg></div>:<div className='menu'>
+              <p>Home</p>
+              <p>About</p>
+              <p>Skills</p>
+              <p>Experience</p>
+              <p>Contact</p>
+          </div>
+            }
          
         </div>
         
@@ -23,7 +26,8 @@ function Hero() {
             <h1 className='text'>I'm Sidhardh, a<br/>Web Developer<br/><span> <p className='sub-text'>I'm passionate about web developmnet.</p></span></h1>
            
           </div>
-           <img src="/home-image-1.png" alt="" /> 
+          {isSmallDevice? <img  style={{width:'15.125rem',height:'15.125rem'}} src="/home-mob-pic.png" alt="" /> : <img src="/home-image-1.png" alt="" /> }
+            
         </div>  
 
       <div className='move-down'>
